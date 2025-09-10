130fc3163e1a0bfcf67b2504565e66ab0cff5fa4
Raihan <raihanuddinahmed10@gmail.com>
2025-09-10 23:42:42 +0600

    state

--- src/components/StateFulForm.jsx
+++ src/components/StateFulForm.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const StateFulForm = () => {
-  return (
-    <div>StateFulForm</div>
-  )
-}
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
 
-export default StateFulForm
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log("Email: ", email);
+    console.log("Password: ", password);
+  };
+
+  // email
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  //   password
+  const handleChangePassword = (e) => {
+    // console.log(e.target.value);
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>State ful Form</h2>
+      <form onSubmit={handleForm}>
+        <input type="text" placeholder="Name" /> <br />
+        <input
+          onChange={handleEmailChange}
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <br />
+        <input
+          onChange={handleChangePassword}
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default StateFulForm;
